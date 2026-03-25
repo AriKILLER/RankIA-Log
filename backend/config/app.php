fedd15d7bc10ef3d3ace7850d569a80580413be2
Arianna <ariannavilchez2@gmail.com>
2026-03-25 14:13:04 +0100

    Cambios en gitignore y en la forma de acceder a las claves de la API para mas seguridad

--- backend/config/app.php
+++ backend/config/app.php
@@ -2,8 +2,8 @@
 return [
     'tmdb' => [
         'url' => 'https://api.themoviedb.org/3',
-        'api_key' => 'bb746fbd67a687fe26e7ec656bcbb20a',
-        'token_acceso' => 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYjc0NmZiZDY3YTY4N2ZlMjZlN2VjNjU2YmNiYjIwYSIsIm5iZiI6MTc3NDM1MjEzMC4yMjcsInN1YiI6IjY5YzI3NzAyN2JlZTRhYzA3MGM3NWU4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Qfdi4yHf1qX4smPDEbDOxE6UMeOZ3oBctMciHIiUlBk',
+        'api_key' => getenv('TMDB_API_KEY') ?: '',
+        'token_acceso' => getenv('TMDB_TOKEN') ?: '',
         'idioma' => 'es-ES',
     ],
 ];
